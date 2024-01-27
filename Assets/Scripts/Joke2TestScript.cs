daa5a029c040e0ed1d896d670166822a52cf7ebe
Raymond Tsai <arethere21@gmail.com>
2024-01-27 13:49:08 -0500

    joke animation setup, randomizer, testing script sequence methods

---
+++ Assets/Scripts/Joke2TestScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joke2TestScript : MonoBehaviour, JokeInterface
+{
+    public string ButtonText { get; private set; }
+
+    public int OccurrenceWeight { get; private set; }
+
+    public GameObject thing;
+
+    public Joke2TestScript()
+    {
+        ButtonText = "lol";
+        OccurrenceWeight = 3;
+    }
+
+
+    public void run()
+    {
+        Debug.Log("Joke2 is in fact funnny");
+
+        StartCoroutine(Joke2Sequence());
+
+        
+    }
+
+    IEnumerator Joke2Sequence()
+    {
+        Debug.Log("tell the joke");
+
+        yield return StartCoroutine(TellJoke2());
+
+        Debug.Log("Joke 2 is completed");
+    }
+
+    IEnumerator TellJoke2()
+    {
+
+        Vector3 startPosition = new Vector3(68.8099976f, 21.0326958f, 38.8139725f);
+
+        Vector3 endPosition = new Vector3(-85.5100021f, -4.04212952f, -16.9272423f);
+
+
+        float timeToAnimate = 1f;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < timeToAnimate)
+        {
+            thing.transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / timeToAnimate);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        thing.transform.position = endPosition;
+
+        Debug.Log("Animation completed");
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/JokeButtonManager.cs
+++ Assets/Scripts/JokeButtonManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,8 @@ public class JokeButtonManager : MonoBehaviour
 
     public JokeInterface[] jokes;
 
+    public AnimationClip[] animations;
+
     //for test
 
     
@@ -18,76 +21,99 @@ public class JokeButtonManager : MonoBehaviour
 
     public Button buttonPrefab;
 
+    public int numJokesPerRound = 5;
+
     private GameObject[] jokeObjects;
+
+    private List<int> alreadyPickedIdx;
 
     // Start is called before the first frame update
     void Start()
     {
 
+        alreadyPickedIdx = new List<int>();
+
+        //In order for this code to recognize and process a joke, the joke script must be
+        //located within an object that is tagged "Joke"
         jokeObjects = GameObject.FindGameObjectsWithTag("Joke");
 
-        returnButtonInstance(jokeObjects[0]);
 
-        /*
-        JokeInterface joke1 = jokeTest.GetComponent<JokeInterface>();
+        //creates the appropriate number of buttons and animates them.
+        for (int i = 0; i < numJokesPerRound; i++)
+        {
+            Button jokeButton = createJokeButton(pickRandomJoke());
 
-        Debug.Log("joke 1: " + joke1);
+            Animator buttonAnimator = jokeButton.GetComponent<Animator>();
 
+            /*
+            int randomIndex = UnityEngine.Random.Range(0, animations.Length);
+
+            AnimationClip clip = animations[randomIndex];
+
+            buttonAnimator.Play(clip.name);*/
+        }
+
+
+    }
+
+    /**
+     * Returns a random joke object from the list of jokes. If all jokes are used up, it will reuse a joke.
+     */
+    private GameObject pickRandomJoke()
+    {
+        System.Random random = new System.Random();
+
+        int randomNumber = random.Next(0, jokeObjects.Length);
+
+        //if all jokes have been used up, clear the already picked list
+        if (alreadyPickedIdx.Count >= jokeObjects.Length)
+        {
+            alreadyPickedIdx.Clear();
+        }
+        //while the joke picked has already been used, pick again
+        while (alreadyPickedIdx.Contains(randomNumber))
+        {
+            randomNumber = random.Next(0, jokeObjects.Length);
+        }
+
+        //add the current joke to the list of jokes already picked
+        alreadyPickedIdx.Add(randomNumber);
+        return jokeObjects[randomNumber];
+    }
+
+    /**
+     * Returns a Button object created from the information stored in the given jokeObject
+     */
+    private Button createJokeButton(GameObject jokeObject)
+    {
+
+        //get the JokeInterface script component from the given joke game object
+        JokeInterface joke = jokeObject.GetComponent<JokeInterface>();
+
+        Debug.Log("joke: " + joke);
+
+        //instantiate the button
         Button myButtonInstance = Instantiate(buttonPrefab, transform);
 
-        Debug.Log("button text: " + joke1.ButtonText);
-        Debug.Log("text child: " + myButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text);
+        Debug.Log("button text: " + joke.ButtonText);
+        //Debug.Log("text child: " + myButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text);
 
-        myButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text = joke1.ButtonText;
+        //change the text of the button
+        myButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text = joke.ButtonText;
 
-        myButtonInstance.onClick.AddListener(joke1.run);*/
+        //add an animator component to the button
+        myButtonInstance.gameObject.AddComponent<Animator>();
 
+        //add the run function from the joke object as a listener to the button click event
+        myButtonInstance.onClick.AddListener(joke.run);
 
-        //myButtonInstance.gameObject.AddComponent<JokeInterfaceComponent>().SetJoke(joke1);
-
-        //myButtonInstance.onClick.AddListener(gameObject.GetComponent<JokeInterfaceComponent>().RunJoke);
+        return myButtonInstance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private Button returnButtonInstance(GameObject jokeObject)
-    {
-        JokeInterface joke = jokeObject.GetComponent<JokeInterface>();
-
-        Debug.Log("joke 1: " + joke);
-
-        Button myButtonInstance = Instantiate(buttonPrefab, transform);
-
-        Debug.Log("button text: " + joke.ButtonText);
-        Debug.Log("text child: " + myButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text);
-
-        myButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text = joke.ButtonText;
-
-        myButtonInstance.onClick.AddListener(joke.run);
-
-        return myButtonInstance;
-    }
 }
-
-/*
-public class JokeInterfaceComponent : MonoBehaviour
-{
-    private JokeInterface jokeInstance;
-
-    public void SetJoke(JokeInterface other)
-    {
-        this.jokeInstance = other;
-    }
-
-    public void RunJoke()
-    {
-        if (jokeInstance != null)
-        {
-            jokeInstance.run();
-        }
-    }
-}*/
